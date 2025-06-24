28bd00c81327f3a1702c09f91482b7c9ab363c9a
oubin <oubin17@gmail.com>
2025-06-24 16:51:52 +0800

    1.添加数据库连接池和Tomcat连接池监控

--- util/src/main/java/com/odk/baseutil/threadpool/ScheduleConfig.java
+++ util/src/main/java/com/odk/baseutil/threadpool/ScheduleConfig.java
@@ -1,7 +1,17 @@
 package com.odk.baseutil.threadpool;
 
+import com.alibaba.druid.pool.DruidDataSource;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.catalina.startup.Tomcat;
+import org.apache.tomcat.util.threads.ThreadPoolExecutor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.context.WebServerApplicationContext;
+import org.springframework.boot.web.embedded.tomcat.TomcatWebServer;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import javax.sql.DataSource;
 
 /**
  * ScheduleConfig
@@ -11,17 +21,42 @@ import org.springframework.context.annotation.Configuration;
  * @version: 1.0
  * @author: oubin on 2025/6/13
  */
-@Slf4j
+@Slf4j(topic = "topic-log")
 @Configuration
 public class ScheduleConfig {
-//
-//    @Async("asyncTaskExecutor")
-//    @Scheduled(cron = "0/5 * * * * ?")
-//    public void blockingTask() throws InterruptedException {
-//        Thread.sleep(5000); // 阻塞5秒
-//        log.info("阻塞 5 秒，合起来 10秒blockingTask");
-//
-//    }
+
+    private DataSource dataSource;
+
+    private WebServerApplicationContext webServerApplicationContext;
+
+    /**
+     * 数据库连接池监控
+     */
+    @Async("asyncTaskExecutor")
+    @Scheduled(cron = "1 * * * * ?")
+    public void dataSourceMonitor() {
+        DruidDataSource druidDataSource =  (DruidDataSource) dataSource;
+        log.info("数据库连接池监控,最大连接数-{},活跃连接数-{},空闲连接数-{},等待连接线程数-{}", druidDataSource.getMaxActive(), druidDataSource.getActiveCount(), druidDataSource.getPoolingCount(), druidDataSource.getWaitThreadCount());
+
+    }
+
+    /**
+     * Tomcat连接池监控
+     */
+    @Async("asyncTaskExecutor")
+    @Scheduled(cron = "11 * * * * ?")
+    public void tomcatMonitor() {
+
+        Tomcat tomcat = ((TomcatWebServer) webServerApplicationContext.getWebServer()).getTomcat();
+        if (tomcat == null || tomcat.getService().findConnectors().length == 0) {
+            return;
+        }
+        ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor) tomcat
+                .getConnector()
+                .getProtocolHandler()
+                .getExecutor();
+        log.info("Tomcat连接池监控,最大线程数-{},活跃线程数-{},阻塞队列长度-{}", threadPoolExecutor.getMaximumPoolSize(), threadPoolExecutor.getActiveCount(), threadPoolExecutor.getQueue().size());
+    }
 //
 //    @Async("asyncTaskExecutor")
 //    @Scheduled(cron = "0/1 * * * * ?")
@@ -30,4 +65,15 @@ public class ScheduleConfig {
 //        log.info("阻塞 1 秒，合起来 2秒blockingTask");
 //
 //    }
+
+
+    @Autowired
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Autowired
+    public void setWebServerApplicationContext(WebServerApplicationContext webServerApplicationContext) {
+        this.webServerApplicationContext = webServerApplicationContext;
+    }
 }
