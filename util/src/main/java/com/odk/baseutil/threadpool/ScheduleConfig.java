28e22f32db9caf56fa52403778e85d026a0ab1b3
oubin <oubin17@gmail.com>
2025-06-26 10:17:40 +0800

    代码优化

--- util/src/main/java/com/odk/baseutil/threadpool/ScheduleConfig.java
+++ util/src/main/java/com/odk/baseutil/threadpool/ScheduleConfig.java
@@ -1,21 +1,12 @@
 package com.odk.baseutil.threadpool;
 
-import com.alibaba.druid.pool.DruidDataSource;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.catalina.startup.Tomcat;
-import org.apache.tomcat.util.threads.ThreadPoolExecutor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.context.WebServerApplicationContext;
-import org.springframework.boot.web.embedded.tomcat.TomcatWebServer;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.Async;
-import org.springframework.scheduling.annotation.Scheduled;
-
-import javax.sql.DataSource;
 
 /**
  * ScheduleConfig
  * 使用@Async支持异步调用，默认的线程池是单线程
+ * 分布式调度需要支持 Quartz 等框架
  *
  * @description:
  * @version: 1.0
@@ -25,38 +16,6 @@ import javax.sql.DataSource;
 @Configuration
 public class ScheduleConfig {
 
-    private DataSource dataSource;
-
-    private WebServerApplicationContext webServerApplicationContext;
-
-    /**
-     * 数据库连接池监控
-     */
-    @Async("asyncTaskExecutor")
-    @Scheduled(cron = "1 * * * * ?")
-    public void dataSourceMonitor() {
-        DruidDataSource druidDataSource =  (DruidDataSource) dataSource;
-        log.info("数据库连接池监控,最大连接数-{},活跃连接数-{},空闲连接数-{},等待连接线程数-{}", druidDataSource.getMaxActive(), druidDataSource.getActiveCount(), druidDataSource.getPoolingCount(), druidDataSource.getWaitThreadCount());
-
-    }
-
-    /**
-     * Tomcat连接池监控
-     */
-    @Async("asyncTaskExecutor")
-    @Scheduled(cron = "11 * * * * ?")
-    public void tomcatMonitor() {
-
-        Tomcat tomcat = ((TomcatWebServer) webServerApplicationContext.getWebServer()).getTomcat();
-        if (tomcat == null || tomcat.getService().findConnectors().length == 0) {
-            return;
-        }
-        ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor) tomcat
-                .getConnector()
-                .getProtocolHandler()
-                .getExecutor();
-        log.info("Tomcat连接池监控,最大线程数-{},活跃线程数-{},阻塞队列长度-{}", threadPoolExecutor.getMaximumPoolSize(), threadPoolExecutor.getActiveCount(), threadPoolExecutor.getQueue().size());
-    }
 //
 //    @Async("asyncTaskExecutor")
 //    @Scheduled(cron = "0/1 * * * * ?")
@@ -67,13 +26,4 @@ public class ScheduleConfig {
 //    }
 
 
-    @Autowired
-    public void setDataSource(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
-
-    @Autowired
-    public void setWebServerApplicationContext(WebServerApplicationContext webServerApplicationContext) {
-        this.webServerApplicationContext = webServerApplicationContext;
-    }
 }
